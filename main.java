0427e0e21a515989f1664a121a535f3afa7d734e
bpntt <haataya@163.com>
2023-09-16 12:08:38 +0600

    first commit

---
+++ main.java
@@ -0,0 +1,24 @@
+ 
+public class main {
+    public static void main(String[] args) {
+
+        BinaryTree<Integer> tree = new BinaryTree<>();
+
+        tree.add(16);
+        tree.add(88);
+        tree.add(28);
+        tree.add(84);
+        tree.add(57);
+        tree.add(10);
+        tree.add(99);
+        tree.add(97);
+        tree.add(51);
+        tree.add(83);
+        tree.add(12);
+        tree.add(15);
+        tree.add(18);
+
+        tree.print();
+
+    }
+}
